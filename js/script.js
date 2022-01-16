8751710e958d10dc7612b84df7d9897c7f00abca
Evgeniy Vybornyi <1vybornyi@gmail.com>
2022-01-16 20:36:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+let incr = 10,
+    decr = 6;
+
+/* incr++;
+decr--; */
+
+console.log(incr);
+console.log(decr);
+
+console.log(2*4 ===8);
+
+
+const isCheked = true;
+      isClose = false;
+
+    console.log(isCheked || !isClose);
